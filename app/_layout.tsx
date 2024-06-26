90b309a7efd24fd4040ea32492ec6a32f47a4c62
varchodi <rauschenrobert@gmail.com>
2024-06-26 19:11:06 +0300

    Fix up same native components build error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,52 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
-import { Stack, useRouter } from "expo-router";
+import { Slot, SplashScreen, Stack, useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SecureStore from 'expo-secure-store'
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
-export default function RootLayout() {
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+// custom token cache 
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+SplashScreen.preventAutoHideAsync()
+
+function InitialLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+
+  // Expo router uses Error boundaries to catch errors in navigation tree 
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  },[loaded])
+
   const router = useRouter();
+  // cleck stuff
+  const { isLoaded, isSignedIn } = useAuth();
+  // const segments
+  if (!loaded || !isLoaded) return <Slot/>
   return (
     <Stack>
       <Stack.Screen name="index" options={{
@@ -14,10 +57,24 @@ export default function RootLayout() {
         title: '',
         headerLeft: () => (
           <TouchableOpacity onPress={()=>router.back()}>
-            <Ionicons name="close-outline" size={18} />
+            <Ionicons name="close-outline" size={28} />
           </TouchableOpacity>
         )
       }}/>
     </Stack>
   );
 }
+
+
+
+const RoutLayoutNav =()=> {
+    return (
+      <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+        <GestureHandlerRootView style={{flex:1}}>
+          <InitialLayout/>
+        </GestureHandlerRootView>
+      </ClerkProvider>
+    )
+}
+
+export default RoutLayoutNav
