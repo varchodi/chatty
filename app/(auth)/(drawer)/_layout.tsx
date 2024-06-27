e0a6160a400883071b69123b5b7b73ebd882878e
varchodi <rauschenrobert@gmail.com>
2024-06-27 17:45:58 +0300

    Drawer: structure file projects

---
+++ app/(auth)/(drawer)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Laout = () => {
+  return (
+    <View>
+      <Text>Laout</Text>
+    </View>
+  )
+}
+
+export default Laout
+
+const styles = StyleSheet.create({})
