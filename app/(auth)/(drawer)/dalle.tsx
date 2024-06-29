14fd1cc918e57608ef8461ac5c8f36c8141d71c1
varchodi <rauschenrobert@gmail.com>
2024-06-29 15:31:28 +0300

    add zeego header on dall-e page , n finish

--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { defaultStyles } from '@/constants/styles'
+import { Stack } from 'expo-router'
+import HeaderDropDown from '@/components/header/HeaderDropDown'
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => <HeaderDropDown title='Dall•E'
+            items={[
+              { key: 'share', title: 'Share GPT', icon: 'square.and.arrow.up' },
+              { key: 'details', title: 'See Details', icon: 'info.circle' },
+              { key: 'keep', title: 'Keep in Sidebar', icon: 'pin' },
+            ]}
+            onSelect={() => {}}
+            selected={''}
+          />,
+          
+        }}
+      />
     </View>
   )
 }
