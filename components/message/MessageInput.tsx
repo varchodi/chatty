755b90dd4c718d816b4b0367724ca75757bd08e8
varchodi <rauschenrobert@gmail.com>
2024-07-07 16:07:06 +0300

    add add btn

--- components/message/MessageInput.tsx
+++ components/message/MessageInput.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Animated, StyleSheet, Text, View,TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useSharedValue } from 'react-native-reanimated';
 import { BlurView } from 'expo-blur';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+
+const ATouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+
 export type MessageInputProps = {
     //?? todo
     onShouldSendMessage: (message: string) => void; 
+}
+
+const expandItems = () => {
+    // todos
 }
 
 const MessageInput = () => {
@@ -16,7 +25,11 @@ const MessageInput = () => {
 
   return (
     <BlurView tint='extraLight' intensity={90} style={{paddingBottom:bottom+5,paddingTop:0}}>
-      <View style={styles.row}></View>
+      <View style={styles.row}>
+        <ATouchableOpacity onPress={expandItems} style={styles.roundBtn}>
+                  <Ionicons name='add' size={24} color={ Colors.grey} />
+        </ATouchableOpacity>
+      </View>
     </BlurView>
   )
 }
@@ -28,6 +41,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 20,
-        backgroundColor:'white'
+    },
+    roundBtn: {
+        width: 30,
+        height: 30,
+        borderRadius: 20,
+        backgroundColor: Colors.input,
+        alignItems: 'center',
+        justifyContent:'center',
     }
 })
