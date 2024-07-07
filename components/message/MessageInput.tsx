e2c8e87efba1a99fd5866e69f7312a156d89239a
varchodi <rauschenrobert@gmail.com>
2024-07-07 14:53:03 +0300

    install same packages

--- components/message/MessageInput.tsx
+++ components/message/MessageInput.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSharedValue } from 'react-native-reanimated';
 
 export type MessageInputProps = {
     //?? todo
@@ -7,9 +9,14 @@ export type MessageInputProps = {
 }
 
 const MessageInput = () => {
+    const [message, setMessage] = useState<string>('');
+    const { bottom } = useSafeAreaInsets();
+    const expanded = useSharedValue(0);
+
+
   return (
     <View>
-      <Text>MessageInput</Text>
+      <Text>MessageInputy</Text>
     </View>
   )
 }
